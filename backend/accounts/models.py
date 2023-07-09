7a7d73bd7e989e37f2b6bc22d5e12489c86b23a6
Jonathan Ramirez <jonksff@gmail.com>
2023-07-08 21:57:52 -0400

    EDIT: way we extend user

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,29 +1,10 @@
-from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin, UserManager
-from django.contrib.auth.validators import ASCIIUsernameValidator
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
 
-
-class CustomUser(AbstractBaseUser, PermissionsMixin):
-    username_validator = ASCIIUsernameValidator()
-
-    username = models.CharField(
-        _("username"),
-        max_length=150,
-        unique=True,
-        help_text=_(
-            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
-        ),
-        validators=[username_validator],
-        error_messages={
-            "unique": _("A user with that username already exists."),
-        },
-    )
-    first_name = models.CharField(_("first name"), max_length=150, blank=False)
+class CustomUser(AbstractUser):
+    first_name = models.CharField(_("first name"), max_length=30, blank=False)
     last_name = models.CharField(_("last name"), max_length=150, blank=False)
     email = models.EmailField(
         _("email address"),
@@ -32,28 +13,12 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
             "unique": _("A user with that email address already exists."),
         },
     )
-    is_staff = models.BooleanField(
-        _("staff status"),
-        default=False,
-        help_text=_("Designates whether the user can log into this admin site."),
-    )
-    is_active = models.BooleanField(
-        _("active"),
-        default=True,
-        help_text=_(
-            "Designates whether this user should be treated as active. Unselect this instead of deleting accounts."
-        ),
-    )
-    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
     avatar = models.ImageField(
         _("user avatar"), upload_to="avatar/", blank=True, null=True
     )
 
-    objects = UserManager()
-
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "username"
-    # What we're going to ask for superusers
     REQUIRED_FIELDS = ["email", "first_name", "last_name"]
 
     class Meta:
@@ -63,8 +28,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def clean(self):
         super().clean()
         self.email = self.__class__.objects.normalize_email(self.email)
-        # make sure all usernames are lowercase hence unique
-        self.username = self.username.lower()
 
     def get_full_name(self):
         """
