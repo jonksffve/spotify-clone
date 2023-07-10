58549cfd529a135469ecb17221a4956455d5f2e8
Jonathan Ramirez <jonksff@gmail.com>
2023-07-10 15:40:14 -0400

    ADD: toastify

---
+++ frontend/src/helpersConfig/toastifyConfig.ts
@@ -0,0 +1,10 @@
+export const toastifyOptions: object = {
+	position: 'bottom-left',
+	autoClose: 4500,
+	hideProgressBar: false,
+	closeOnClick: true,
+	pauseOnHover: true,
+	draggable: true,
+	progress: undefined,
+	theme: 'dark',
+};
