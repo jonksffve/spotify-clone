70ff6b115ba9007040f17813c4f24ecdb29cf7a6
Jonathan Ramirez <jonksff@gmail.com>
2023-07-10 12:55:19 -0400

    EDIT: url pattern

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("account/", views.UserCreationView.as_view())]
+urlpatterns = [
+    path("account/", views.UserCreationView.as_view()),
+    # path("account/<str:id>/", views.UserCreationView.as_view()),
+    # path("account/", views.UserCreationView.as_view()),
+]
