42d27e61a4558032b3035dffef15c10ad7dfc197
Jonathan Ramirez <jonksff@gmail.com>
2023-07-10 18:12:08 -0400

    ADD: auth token endpoint

--- backend/api/tests/setup.py
+++ backend/api/tests/setup.py
@@ -6,6 +6,7 @@ from django.contrib.auth import get_user_model
 class EndpointTestingSetup(APITransactionTestCase):
     def setUp(self):
         self.endpoint_account = "/api/account/"
+        self.endpoint_account_auth = "/api/account/token-auth/"
         self.user_model = get_user_model()
         self.client = APIClient()
 
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("account/", views.UserCreationView.as_view()),
+    path("account/token-auth/", views.CustomAuthToken.as_view()),
     # path("account/<str:id>/", views.UserCreationView.as_view()),
     # path("account/", views.UserCreationView.as_view()),
 ]
