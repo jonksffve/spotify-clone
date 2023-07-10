75d80990ea2fe93e0602671ebbc44d7c59ed1506
Jonathan Ramirez <jonksff@gmail.com>
2023-07-10 18:14:16 -0400

    ADD: get_user_model
    ADD: customauthtoken view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,33 @@
-from accounts.models import CustomUser
+from django.contrib.auth import get_user_model
+
 from accounts.serializers import UserCreateSerializer
+from accounts.serializers import UserListSerializer
+
 from rest_framework.generics import CreateAPIView
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+user_model = get_user_model()
 
 
 class UserCreationView(CreateAPIView):
-    queryset = CustomUser.objects.all()
+    queryset = user_model.objects.all()
     serializer_class = UserCreateSerializer
+
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, created = Token.objects.get_or_create(user=user)
+        user_serializer = UserListSerializer(user, context={"request": request})
+        return Response(
+            {
+                "token": token.key,
+                "user": user_serializer.data,
+            }
+        )
