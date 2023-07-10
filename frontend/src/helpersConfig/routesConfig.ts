d38cd5217ac528e7817e87e05d071075996f0b4b
Jonathan Ramirez <jonksff@gmail.com>
2023-07-10 15:40:31 -0400

    ADD: url endpoint account

--- frontend/src/helpersConfig/routesConfig.ts
+++ frontend/src/helpersConfig/routesConfig.ts
@@ -1,2 +1,6 @@
+//BACKEND URL ENDPOINTS
+export const ENDPOINT_ACCOUNT = 'http://localhost:8000/api/account/';
+
+//APP ENDPOINTS
 export const ROUTE_HOME = '/';
 export const ROUTE_SEARCH = '/search/';
