e4582accf970139800cee0280291ac2657a81168
Jonathan Ramirez <jonksff@gmail.com>
2023-07-10 20:41:28 -0400

    ADD: token auth endpoint

--- frontend/src/helpersConfig/routesConfig.ts
+++ frontend/src/helpersConfig/routesConfig.ts
@@ -1,5 +1,7 @@
 //BACKEND URL ENDPOINTS
 export const ENDPOINT_ACCOUNT = 'http://localhost:8000/api/account/';
+export const ENDPOINT_TOKEN_AUTH =
+	'http://localhost:8000/api/account/token-auth/';
 
 //APP ENDPOINTS
 export const ROUTE_HOME = '/';
