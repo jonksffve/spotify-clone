27f072c159ccd8d298260b6a49c5257cc18e4916
Jonathan Ramirez <jonksff@gmail.com>
2023-07-13 11:11:04 -0400

    ADD: upload song endpoint

--- frontend/src/helpersConfig/routesConfig.ts
+++ frontend/src/helpersConfig/routesConfig.ts
@@ -2,6 +2,7 @@
 export const ENDPOINT_ACCOUNT = 'http://localhost:8000/api/account/';
 export const ENDPOINT_TOKEN_AUTH =
 	'http://localhost:8000/api/account/token-auth/';
+export const ENDPOINT_SONG = 'http://localhost:8000/api/song/';
 
 //APP ENDPOINTS
 export const ROUTE_HOME = '/';
