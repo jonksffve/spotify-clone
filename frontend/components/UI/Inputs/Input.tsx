e983a25a11a30ce8f1280f8d72a6b3aec1f50e74
Jonathan Ramirez <jonksff@gmail.com>
2023-07-14 10:33:27 -0400

    EDIT: icon now optional

--- frontend/components/UI/Inputs/Input.tsx
+++ frontend/components/UI/Inputs/Input.tsx
@@ -2,23 +2,25 @@ import { forwardRef } from 'react';
 import { IconType } from 'react-icons';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-	icon: IconType;
+	icon?: IconType;
 }
 
 const Input = forwardRef<HTMLInputElement, InputProps>(
 	({ type = 'text', icon: Icon, ...props }, ref) => {
 		return (
-			<div className="flex items-center w-full relative">
+			<div className='flex items-center w-full relative'>
 				<input
 					type={type}
-					className="w-full rounded-md bg-neutral-700 border border-transparent px-3 py-3 text-sm file:border-[1px] file:text-sm file:font-medium placeholder:text-neutral-400"
+					className='w-full rounded-md bg-neutral-700 border border-transparent px-3 py-3 text-sm file:border-[1px] file:text-sm file:font-medium placeholder:text-neutral-400'
 					ref={ref}
 					{...props}
 				/>
-				<Icon
-					className="absolute right-1"
-					size={20}
-				/>
+				{Icon && (
+					<Icon
+						className='absolute right-1'
+						size={20}
+					/>
+				)}
 			</div>
 		);
 	}
