1169cf694ecd05f609c1156824a1234a62b370d6
Jonathan Ramirez <jonksff@gmail.com>
2023-07-14 10:33:11 -0400

    ADD: endpoint songlike

--- frontend/src/helpersConfig/routesConfig.ts
+++ frontend/src/helpersConfig/routesConfig.ts
@@ -3,6 +3,7 @@ export const ENDPOINT_ACCOUNT = 'http://localhost:8000/api/account/';
 export const ENDPOINT_TOKEN_AUTH =
 	'http://localhost:8000/api/account/token-auth/';
 export const ENDPOINT_SONG = 'http://localhost:8000/api/song/';
+export const ENDPOINT_SONG_LIKE = 'http://localhost:8000/api/song/like/';
 
 //APP ENDPOINTS
 export const ROUTE_HOME = '/';
