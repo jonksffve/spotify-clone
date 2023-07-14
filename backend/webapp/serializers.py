89c3c3e0d213187ce61d7e163144576c1889908e
Jonathan Ramirez <jonksff@gmail.com>
2023-07-14 10:29:41 -0400

    ADD: songlistserializer, songlikeserializer

--- backend/webapp/serializers.py
+++ backend/webapp/serializers.py
@@ -1,8 +1,26 @@
-from rest_framework.serializers import ModelSerializer
-from .models import SongFile
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from .models import SongFile, SongLike
 
 
 class SongCreateSerializer(ModelSerializer):
     class Meta:
         model = SongFile
         fields = ["title", "song_author", "song_file", "cover_image"]
+
+
+class SongListSerializer(ModelSerializer):
+    is_liked = SerializerMethodField()
+
+    class Meta:
+        model = SongFile
+        fields = ["id", "title", "song_author", "song_file", "cover_image", "is_liked"]
+
+    def get_is_liked(self, obj):
+        logged_user = self.context["request"].user
+        return obj.likes.filter(user=logged_user).exists()
+
+
+class SongLikeSerializer(ModelSerializer):
+    class Meta:
+        model = SongLike
+        fields = ["user", "song"]
