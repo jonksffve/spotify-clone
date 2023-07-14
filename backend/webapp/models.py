601849de3015f04ebf665606faed7026936a893e
Jonathan Ramirez <jonksff@gmail.com>
2023-07-13 21:44:04 -0400

    EDIT: fomatting
    ADD: date_uploaded and ordering

--- backend/webapp/models.py
+++ backend/webapp/models.py
@@ -3,16 +3,29 @@ from django.utils.translation import gettext_lazy as _
 
 
 class SongFile(models.Model):
-    user = models.ForeignKey("accounts.CustomUser", verbose_name=_(
-        "Uploader"), on_delete=models.CASCADE)
-    title = models.CharField(
-        _("Song title"), max_length=50, blank=False, null=False)
+    user = models.ForeignKey(
+        "accounts.CustomUser", verbose_name=_("Uploader"), on_delete=models.CASCADE
+    )
+    title = models.CharField(_("Song title"), max_length=50, blank=False, null=False)
     song_author = models.CharField(
-        _("Song author"), max_length=50, blank=False, null=False)
+        _("Song author"), max_length=50, blank=False, null=False
+    )
     song_file = models.FileField(
-        _("Song"), upload_to="songs/", max_length=100, blank=False, null=False)
+        _("Song"), upload_to="songs/", max_length=100, blank=False, null=False
+    )
     cover_image = models.ImageField(
-        _("Song cover image"), upload_to="album/", default="music-placeholder.jfif", blank=True, null=True)
+        _("Song cover image"),
+        upload_to="album/",
+        default="music-placeholder.jfif",
+        blank=True,
+        null=True,
+    )
+    date_uploaded = models.DateTimeField(
+        _("Upload date"), auto_now=False, auto_now_add=True
+    )
+
+    class Meta:
+        ordering = ["-date_uploaded"]
 
     def __str__(self):
         return f"{self.title} by {self.song_author}"
