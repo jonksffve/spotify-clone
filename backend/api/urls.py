6ded33a33243c30b39ee2ce189668a2f36f4b574
Jonathan Ramirez <jonksff@gmail.com>
2023-07-14 10:31:13 -0400

    ADD: songlikecreate, songlikedestroy Views/urls
    EDIT: songcreate serializer based on requestmethod

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("account/token-auth/", views.CustomAuthToken.as_view()),
     path("account/<str:key>/", views.UserRetrieveView.as_view()),
     path("song/", views.SongCreateView.as_view()),
-    # path("account/", views.UserCreationView.as_view()),
+    path("song/like/", views.SongLikeCreateView.as_view()),
+    path("song/like/<str:song>/", views.SongLikeDestroyView.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -5,15 +5,25 @@ from accounts.serializers import (
     UserListSerializer,
     TokenSerializer,
 )
-from webapp.models import SongFile
-from webapp.serializers import SongCreateSerializer
+from webapp.models import SongFile, SongLike
+from webapp.serializers import (
+    SongCreateSerializer,
+    SongListSerializer,
+    SongLikeSerializer,
+)
 
 
-from rest_framework.generics import CreateAPIView, RetrieveAPIView, ListCreateAPIView
+from rest_framework.generics import (
+    CreateAPIView,
+    RetrieveAPIView,
+    ListCreateAPIView,
+    DestroyAPIView,
+)
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 user_model = get_user_model()
 
@@ -50,12 +60,53 @@ class CustomAuthToken(ObtainAuthToken):
 class SongCreateView(ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = None
-    serializer_class = SongCreateSerializer
+    serializer_class = None
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return SongCreateSerializer
+        return SongListSerializer
 
     def get_queryset(self):
+        if "title" in self.request.query_params:
+            return SongFile.objects.filter(
+                title__icontains=self.request.query_params["title"]
+            )
         if "user_only" in self.request.query_params:
             return SongFile.objects.filter(user=self.request.user)
         return SongFile.objects.all()
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class SongLikeCreateView(CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = SongLike.objects.all()
+    serializer_class = SongLikeSerializer
+
+    # 1 like per user to the same song
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(
+            data={"song": request.data["song"], "user": request.user.id}
+        )
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+
+class SongLikeDestroyView(DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = None
+    serializer_class = SongLikeSerializer
+    lookup_field = "song"
+
+    # destroy IF its yours
+    # destroy if it exists
+
+    def get_queryset(self):
+        return SongLike.objects.filter(user=self.request.user)
