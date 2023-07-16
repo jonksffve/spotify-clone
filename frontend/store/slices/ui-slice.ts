840c607848c3cbe02ad76e2159770148d17daa68
Jonathan Ramirez <jonksff@gmail.com>
2023-07-16 04:02:22 -0400

    ADD: music player state

--- frontend/store/slices/ui-slice.ts
+++ frontend/store/slices/ui-slice.ts
@@ -1,15 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { Song } from '../../src/helpersConfig/types';
 
 interface UiState {
 	showLoginModal: boolean;
 	showRegisterModal: boolean;
 	showUploadModal: boolean;
+	showMusicPlayer: boolean;
+	song: Song | undefined;
+	playlist: Song[] | undefined;
 }
 
 const initialState: UiState = {
 	showLoginModal: false,
 	showRegisterModal: false,
 	showUploadModal: false,
+	showMusicPlayer: false,
+	song: undefined,
+	playlist: undefined,
 };
 
 const uiSlice = createSlice({
@@ -33,6 +41,18 @@ const uiSlice = createSlice({
 		},
 		closeUploadModal: (state) => {
 			state.showUploadModal = false;
+		},
+		showMusicPlayer: (state) => {
+			state.showMusicPlayer = true;
+		},
+		closeMusicPlayer: (state) => {
+			state.showMusicPlayer = false;
+		},
+		setPlayerSong: (state, action: PayloadAction<Song>) => {
+			state.song = action.payload;
+		},
+		setPlayerPlaylist: (state, action: PayloadAction<Song[]>) => {
+			state.playlist = action.payload;
 		},
 	},
 });
